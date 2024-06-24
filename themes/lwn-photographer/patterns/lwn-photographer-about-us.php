bf78822b531ba9b0dd2eb1b37040d446f4b2e975
Nawras <nawras.othman@yandex.com>
2024-06-24 18:50:14 +0300

    add about us pattern

---
+++ themes/lwn-photographer/patterns/lwn-photographer-about-us.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Title: LWN Photographer About Us
+ * Slug: lwn-photographer/lwn-photographer-about-us
+ * */
+?>
+
+
+<!-- wp:group {"metadata":{"name":"about us "},"style":{"color":{"background":"#e9e9e9"},"spacing":{"padding":{"top":"90px","bottom":"90px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group has-background" style="background-color:#e9e9e9;padding-top:90px;padding-bottom:90px"><!-- wp:group {"metadata":{"name":"about us container"},"className":"lwn-photographer-front-page-about-us-container","layout":{"type":"default"}} -->
+<div class="wp-block-group lwn-photographer-front-page-about-us-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"metadata":{"name":"content"},"style":{"color":{"background":"#ffffff8c"},"spacing":{"padding":{"top":"60px","bottom":"60px","left":"80px","right":"80px"}}},"className":"lwn-photographer-front-page-about-us-content","layout":{"type":"constrained"}} -->
+<div class="wp-block-group lwn-photographer-front-page-about-us-content has-background" style="background-color:#ffffff8c;padding-top:60px;padding-right:80px;padding-bottom:60px;padding-left:80px"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<h2 class="wp-block-heading has-contrast-color has-text-color has-link-color">______________ حول الموقع</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"large"} -->
+<p class="has-base-color has-text-color has-link-color has-large-font-size">أنا <strong>مصور</strong> مبدع مقيم في نيويورك ، الولايات المتحدة الأمريكية</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>عنصر واحد</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>عنصر اثنين</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>عنصر ثلاثة</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"medium"} -->
+<p class="has-base-color has-text-color has-link-color has-medium-font-size">هذا نص نص اضافي ، اضف هنا ما تريد. هذا نص نص اضافي ، اضف هنا ما تريد هذا نص نص اضافي ، اضف هنا ما تريد </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"contrast","textColor":"base","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"border":{"radius":"0px"}},"className":"is-style-lwn-photographer-button"} -->
+<div class="wp-block-button is-style-lwn-photographer-button"><a class="wp-block-button__link has-base-color has-contrast-background-color has-text-color has-background has-link-color wp-element-button" style="border-radius:0px">تواصل معي</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
